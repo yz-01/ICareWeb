37cc54cd3223b83d09f0d9bc9b7383402742945b
albinkoh <albin@oceantech.asia>
2023-02-28 15:33:22 +0800

    Agent Login Register Forgot Reset Password Done

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -18,10 +19,27 @@ class Agent extends Authenticatable
 
     protected $hidden = ['password', 'remember_token',];
 
+    public function sendPasswordResetNotification($token): void
+    {
+        $this->notify(new ResetPassword($token, 'agent'));
+    }
+
     public function country()
     {
         return $this->belongsTo(Country::class)->withDefault([
             'name' => '-',
         ]);
+    }
+
+    public function security_question()
+    {
+        return $this->belongsTo(SecurityQuestion::class)->withDefault([
+            'name' => '-',
+        ]);
+    }
+
+    public function point_transactions()
+    {
+        return $this->hasMany(PointTransaction::class);
     }
 }
